b16361a18c445085be3e01abd3a5f9dd28c9b6c5
BOK-YEOL JUNG 정복열 차장 두산로지스틱스솔루션 <bok-yeol.jung@doosan.com>
2023-03-14 16:18:13 +0900

    '결제해서진행하는거'

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(8080, function() {
+    console.log('listening on 8080')
+})
+
+app.get('/', function(요청, 응답) { 
+  응답.sendFile(__dirname +'/index.html')
+})
+
+app.get('/write', function(요청, 응답) { 
+    응답.sendFile(__dirname +'/write.html')
+});
+
+app.post('/add', function(요청, 응답){
+  console.log(요청.body);
+  응답.send('전송완료')
+});
+
