d64aec9e9c4e3deca1a52a597ba564ad7dddec0b
BOK-YEOL JUNG 정복열 차장 두산로지스틱스솔루션 <bok-yeol.jung@doosan.com>
2023-03-15 16:37:06 +0900

    몽고DB까지 추가해서 진행하는거 성공

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const bodyParser= require('body-parser')
-app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(8080, function() {
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb+srv://todoapp:todoapp@cluster0.fg8v9nz.mongodb.net/?retryWrites=true&w=majority";
+
+MongoClient.connect(uri, function(에러, client){
+  if (에러) return console.log(에러);
+
+  const db = client.db("todoapp");
+
+  const collection = db.collection('post');
+
+  collection.insertOne({이름 : 'John', 나이 : 20}, function(에러, 결과){
+    console.log('저장완료');
+  });
+
+
+  app.listen(8080, function() {
     console.log('listening on 8080')
+  });
 })
+
 
 app.get('/', function(요청, 응답) { 
   응답.sendFile(__dirname +'/index.html')
